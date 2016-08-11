74e01bc8421b93ad15f8b0b3307ab4e5d935d97f
manjula <manjula.dube@craftsvilla.com>
2016-08-11 15:38:59 +0530

    change the image every 3 second

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,17 @@
 (function(){
+	var imageList = [
+		'img/bg1.jpg' , 'img/bg2.jpg' , 'img/bg3.jpg'
+	];
 
-})()
+	function initImageChange(){
+		var _counter = 0 ;
+		setInterval(function(){
+			$("#container-images").find("img").attr('src' , imageList[_counter]);
+			_counter++;
+			_counter = _counter % imageList.length;
+		},3000);
+	}
+
+	initImageChange();
+
+})();
